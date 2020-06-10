e7ad049a64c626f80686dc34e3e24fa6e8fc0c8a
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-06-10 16:43:12 +0200

    r Use contexts instead of describe

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,34 +1,36 @@
 require 'project'
 
 describe 'Gossiping Bus Drivers' do
-  describe 'when the drivers never meet' do
+  context 'when the drivers never meet' do
     it 'answers :never' do
       number_of_stops = count_stops_to_exchange_all_gossips([[1],[2]])
       expect(number_of_stops).to eq :never
     end
   end
 
-  describe 'when all the drivers start from the same stop' do
-    it 'exchanges all gossips on the first stop' do
-      number_of_stops = count_stops_to_exchange_all_gossips([[1],[1]])
-      expect(number_of_stops).to eq 1
-    end
-  end
-
-  describe 'when all the drivers do not start from the same stop' do
-    it 'exchanges all gossips on the last stop' do
-      number_of_stops = count_stops_to_exchange_all_gossips([[1,2],[3,2]])
-      expect(number_of_stops).to eq 2
+  context 'when two drivers meet on first bus tour' do
+    context 'when all the drivers start from the same stop' do
+      it 'exchanges all gossips on the first stop' do
+        number_of_stops = count_stops_to_exchange_all_gossips([[1],[1]])
+        expect(number_of_stops).to eq 1
+      end
     end
 
-    it 'exchanges all gossips on the first stop in common' do
-      number_of_stops = count_stops_to_exchange_all_gossips([[1,2,5],[3,2,4]])
-      expect(number_of_stops).to eq 2
-    end
+    context 'when all the drivers do not start from the same stop' do
+      it 'exchanges all gossips on the last stop' do
+        number_of_stops = count_stops_to_exchange_all_gossips([[1,2],[3,2]])
+        expect(number_of_stops).to eq 2
+      end
 
-    it 'exchanges all gossips on the first stop where they meet' do
-      number_of_stops = count_stops_to_exchange_all_gossips([[1,2,5],[2,1,5]])
-      expect(number_of_stops).to eq 3
+      it 'exchanges all gossips on the first stop in common' do
+        number_of_stops = count_stops_to_exchange_all_gossips([[1,2,5],[3,2,4]])
+        expect(number_of_stops).to eq 2
+      end
+
+      it 'exchanges all gossips on the first stop where they meet' do
+        number_of_stops = count_stops_to_exchange_all_gossips([[1,2,5],[2,1,5]])
+        expect(number_of_stops).to eq 3
+      end
     end
   end
 end

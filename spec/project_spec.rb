3a5aa7e17ce04cee73dcdffc4c6401bddd410655
ARJ <ajoanny@octo.com>
2020-06-24 14:30:30 +0200

    f add the maximum number of stop until driver can meet

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -43,6 +43,23 @@ describe 'Gossiping Bus Drivers' do
         )
         expect(number_of_stops).to eq 4
       end
+
+      it 'exchanges all gossips on the first stop during any bus tour' do
+        number_of_stops = count_stops_to_exchange_all_gossips([
+                                                                  [1,2],
+                                                                  [2,3,4]]
+        )
+        expect(number_of_stops).to eq 4
+      end
+
+      context 'when the drivers meet before the 481th stops' do
+        it 'exchanges all gossips during the stop' do
+          driver_1_stops = [3,1]
+          driver_2_stops = Array.new(479).fill(2).push(1)
+          number_of_stops = count_stops_to_exchange_all_gossips([driver_1_stops, driver_2_stops])
+          expect(number_of_stops).to eq 480
+        end
+      end
     end
   end
 end

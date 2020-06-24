9cf6f66bb51ddd3c02786705945effb709136f6e
ARJ <ajoanny@octo.com>
2020-06-24 14:16:26 +0200

    r organize and rename tests

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -8,39 +8,41 @@ describe 'Gossiping Bus Drivers' do
     end
   end
 
-  context 'when two drivers meet on first bus tour' do
-    context 'when all the drivers start from the same stop' do
+  context 'when there are two driver' do
+    context 'when drivers have the same number of stops' do
+      context 'when drivers start from the same stop' do
+        it 'exchanges all gossips on the first stop' do
+          number_of_stops = count_stops_to_exchange_all_gossips([[1],[1]])
+          expect(number_of_stops).to eq 1
+        end
+      end
+
+      context 'when drivers do not start from the same stop' do
+        it 'exchanges all gossips on the last stop' do
+          number_of_stops = count_stops_to_exchange_all_gossips([[1,2],[3,2]])
+          expect(number_of_stops).to eq 2
+        end
+
+        it 'exchanges all gossips on the first stop in common' do
+          number_of_stops = count_stops_to_exchange_all_gossips([[1,2,5],[3,2,4]])
+          expect(number_of_stops).to eq 2
+        end
+
+        it 'exchanges all gossips on the first stop where they meet' do
+          number_of_stops = count_stops_to_exchange_all_gossips([[1,2,5],[2,1,5]])
+          expect(number_of_stops).to eq 3
+        end
+      end
+    end
+
+    context 'when two drivers have not the same number of stops' do
       it 'exchanges all gossips on the first stop' do
-        number_of_stops = count_stops_to_exchange_all_gossips([[1],[1]])
-        expect(number_of_stops).to eq 1
+        number_of_stops = count_stops_to_exchange_all_gossips([
+          [1,2],
+          [2,3,4]]
+        )
+        expect(number_of_stops).to eq 4
       end
-    end
-
-    context 'when all the drivers do not start from the same stop' do
-      it 'exchanges all gossips on the last stop' do
-        number_of_stops = count_stops_to_exchange_all_gossips([[1,2],[3,2]])
-        expect(number_of_stops).to eq 2
-      end
-
-      it 'exchanges all gossips on the first stop in common' do
-        number_of_stops = count_stops_to_exchange_all_gossips([[1,2,5],[3,2,4]])
-        expect(number_of_stops).to eq 2
-      end
-
-      it 'exchanges all gossips on the first stop where they meet' do
-        number_of_stops = count_stops_to_exchange_all_gossips([[1,2,5],[2,1,5]])
-        expect(number_of_stops).to eq 3
-      end
-    end
-  end
-
-  context 'when two drivers have not the same number of stops'do
-    it 'exchanges all gossips on the first stop' do
-      number_of_stops = count_stops_to_exchange_all_gossips([
-        [1,2],
-        [2,3,4]]
-      )
-      expect(number_of_stops).to eq 4
     end
   end
 end
